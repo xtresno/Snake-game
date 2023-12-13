3b776e206268c50c4f3cdc8db6d680568bb86605
tristan12345676 <decentralizedduckden@gmail.com>
2023-12-13 08:47:09 -0500

    added restart with space, changed colors of snake

--- GamePanel.java
+++ GamePanel.java
@@ -3,12 +3,10 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-import javax.swing.JPanel;
-
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	static final int WIDTH = 500;
 	static final int HEIGHT = 500;
 	static final int UNIT_SIZE = 20;
@@ -17,7 +15,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	// hold x and y coordinates for body parts of the snake
 	final int x[] = new int[NUMBER_OF_UNITS];
 	final int y[] = new int[NUMBER_OF_UNITS];
-	
+
 	// initial length of the snake
 	int length = 5;
 	int foodEaten;
@@ -27,7 +25,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	boolean running = false;
 	Random random;
 	Timer timer;
-	
+
 	GamePanel() {
 		random = new Random();
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -35,29 +33,29 @@ public class GamePanel extends JPanel implements ActionListener{
 		this.setFocusable(true);
 		this.addKeyListener(new MyKeyAdapter());
 		play();
-	}	
-	
+	}
+
 	public void play() {
 		addFood();
 		running = true;
-		
+
 		timer = new Timer(80, this);
-		timer.start();	
+		timer.start();
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics graphics) {
 		super.paintComponent(graphics);
 		draw(graphics);
 	}
-	
+
 	public void move() {
 		for (int i = length; i > 0; i--) {
 			// shift the snake one unit to the desired direction to create a move
-			x[i] = x[i-1];
-			y[i] = y[i-1];
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
 		}
-		
+
 		if (direction == 'L') {
 			x[0] = x[0] - UNIT_SIZE;
 		} else if (direction == 'R') {
@@ -66,46 +64,46 @@ public class GamePanel extends JPanel implements ActionListener{
 			y[0] = y[0] - UNIT_SIZE;
 		} else {
 			y[0] = y[0] + UNIT_SIZE;
-		}	
+		}
 	}
-	
+
 	public void checkFood() {
-		if(x[0] == foodX && y[0] == foodY) {
+		if (x[0] == foodX && y[0] == foodY) {
 			length++;
 			foodEaten++;
 			addFood();
 		}
 	}
-	
+
 	public void draw(Graphics graphics) {
-		
 		if (running) {
-			graphics.setColor(new Color(210, 115, 90));
+			graphics.setColor(Color.YELLOW); // Changed food color to yellow
 			graphics.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
-			
-			graphics.setColor(Color.white);
-			graphics.fillRect(x[0], y[0], UNIT_SIZE, UNIT_SIZE);
-			
-			for (int i = 1; i < length; i++) {
-				graphics.setColor(new Color(40, 200, 150));
+
+			for (int i = 0; i < length; i++) {
+				if (i == 0) {
+					graphics.setColor(Color.GREEN); // Changed head color to green
+				} else {
+					graphics.setColor(Color.BLUE); // Changed body color to blue
+				}
 				graphics.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 			}
-			
-			graphics.setColor(Color.white);
-			graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
+
+			graphics.setColor(Color.WHITE);
+			graphics.setFont(new Font("Sans serif", Font.BOLD, 25));
 			FontMetrics metrics = getFontMetrics(graphics.getFont());
-			graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2, graphics.getFont().getSize());
-		
+			graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2,
+					graphics.getFont().getSize());
 		} else {
 			gameOver(graphics);
 		}
 	}
-	
+
 	public void addFood() {
-		foodX = random.nextInt((int)(WIDTH / UNIT_SIZE))*UNIT_SIZE;
-		foodY = random.nextInt((int)(HEIGHT / UNIT_SIZE))*UNIT_SIZE;
+		foodX = random.nextInt((int) (WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		foodY = random.nextInt((int) (HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 	}
-	
+
 	public void checkHit() {
 		// check if head run into its body
 		for (int i = length; i > 0; i--) {
@@ -113,30 +111,32 @@ public class GamePanel extends JPanel implements ActionListener{
 				running = false;
 			}
 		}
-		
+
 		// check if head run into walls
 		if (x[0] < 0 || x[0] > WIDTH || y[0] < 0 || y[0] > HEIGHT) {
 			running = false;
 		}
-		
-		if(!running) {
+
+		if (!running) {
 			timer.stop();
 		}
 	}
-	
+
 	public void gameOver(Graphics graphics) {
 		graphics.setColor(Color.red);
 		graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 50));
 		FontMetrics metrics = getFontMetrics(graphics.getFont());
 		graphics.drawString("Game Over", (WIDTH - metrics.stringWidth("Game Over")) / 2, HEIGHT / 2);
-		
+		graphics.drawString("Press Space to Restart!", (WIDTH - metrics.stringWidth("Press Space to Restart!")) / 3, HEIGHT / 4);
+
 		graphics.setColor(Color.white);
 		graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
 		metrics = getFontMetrics(graphics.getFont());
-		graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2, graphics.getFont().getSize());
+		graphics.drawString("Score: " + foodEaten, (WIDTH - metrics.stringWidth("Score: " + foodEaten)) / 2,
+				graphics.getFont().getSize());
 
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if (running) {
@@ -146,75 +146,50 @@ public class GamePanel extends JPanel implements ActionListener{
 		}
 		repaint();
 	}
-	
+
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			switch(e.getKeyCode()) {
+			switch (e.getKeyCode()) {
 				case KeyEvent.VK_LEFT:
 					if (direction != 'R') {
 						direction = 'L';
 					}
 					break;
-					
+
 				case KeyEvent.VK_RIGHT:
 					if (direction != 'L') {
 						direction = 'R';
 					}
 					break;
-					
+
 				case KeyEvent.VK_UP:
 					if (direction != 'D') {
 						direction = 'U';
 					}
 					break;
-					
+
 				case KeyEvent.VK_DOWN:
 					if (direction != 'U') {
 						direction = 'D';
 					}
-					break;		
+				case KeyEvent.VK_SPACE:
+					if (!running) {
+						resetGame();
+					}
+					break;
 			}
 		}
 	}
+
+	public void resetGame() {
+		length = 5;
+		foodEaten = 0;
+		direction = 'D';
+		for (int i = 0; i < length; i++) {
+			x[i] = 0;
+			y[i] = 0;
+		}
+		play();
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
